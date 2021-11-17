c5475b01c2d016b57dfe3f72f92a8736b16cb8dd
Viktoriya Rimchuk <rimv@abmcloud.com>
2021-11-17 09:58:47 +0200

    add file

---
+++ lab-catalog/main.js
@@ -0,0 +1,20 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    
+    document.getElementById("myDropdown").classList.toggle("show");
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.drpdwn')) {
+
+    var dropdowns = document.getElementsByClassName("menu-list");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+}
